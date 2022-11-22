b9d44eb52904ea3dfec3b8470ac196a4a451d23b
Despacita21 <lan20a@acu.edu>
2022-11-21 21:18:34 -0600

    Initial Commit

---
+++ Roll Again V2-LN/Roll_Again_V2_LNApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_Again_V2_LNApp.swift
+//  Roll Again V2-LN
+//
+//  Created by Lauren Najera on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Roll_Again_V2_LNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
